37d9532864f66aa2c6b4e0f107f83f5bc5644f93
bkorumaz <bahadir.korumaz@gmail.com>
2020-03-17 19:48:36 +0300

    added media queries with radium library with errors

--- src/App.js
+++ src/App.js
@@ -1,55 +1,72 @@
-import React, { useState } from 'react';
-import './App.css';
-import Radium from 'radium';
+import React, { useState } from "react";
+import "./App.css";
+import Radium, { StyleRoot } from "radium";
 
 function App() {
-
   const [showIndex, setShowIndex] = useState(true);
   const [isRed, setRed] = useState(false);
   const [isBold, setBold] = useState(false);
-  
+
   const style = {
-    backgroundColor: 'green',
-    color: 'white',
-    font: 'inherit',
-    border: '1px solid blue',
-    padding: '8px',
-    cursor: 'pointer',
-    ':hover': {
-      backgroundColor: 'lightgreen',
-      color: 'black'
+    backgroundColor: "green",
+    color: "white",
+    font: "inherit",
+    border: "1px solid blue",
+    padding: "8px",
+    cursor: "pointer",
+    ":hover": {
+      backgroundColor: "lightgreen",
+      color: "black"
     }
   };
 
-  let index = null
+  let index = null;
 
-  if(showIndex) {
-    style.backgroundColor = 'red';
-    style[':hover']= {
-      backgroundColor: 'lightred',
-      color: 'black'
+  if (showIndex) {
+    style.backgroundColor = "red";
+    style[":hover"] = {
+      backgroundColor: "lightred",
+      color: "black"
     };
     index = (
-      <div>
-        <p className={`${isRed ? 'red' : ''} ${isBold ? 'bold' : ''}`}>This is Index</p>
-        <button onClick={() => setRed(!isRed)}><div className='red'>Red</div></button><button onClick={() => setBold(!isBold)}><div className='bold'>Bold</div></button>
+      <div className='index' style={styleMedia}>
+        <p className={`${isRed ? "red" : ""} ${isBold ? "bold" : ""}`}>
+          This is Index
+        </p>
+        <button onClick={() => setRed(!isRed)}>
+          <div className="red">Red</div>
+        </button>
+        <button onClick={() => setBold(!isBold)}>
+          <div className="bold">Bold</div>
+        </button>
       </div>
-      );
+    );
   }
 
   const handleToggleIndex = () => setShowIndex(!showIndex);
-  
+
+  function Index() {
+    const styleMedia = {
+      '@media (min-width: 500px)': {
+        width: '400px'
+      }
+    };
+
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <button style={style} onClick={handleToggleIndex}>
+          Toggle the Index
+        </button>
+        {index}
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <button
-      style={style}
-      onClick={handleToggleIndex}
-      >
-        Toggle the Index
-      </button>
-      {index}
-    </div>
+    <StyleRoot>
+      <Index />
+    </StyleRoot>
   );
 }
 
